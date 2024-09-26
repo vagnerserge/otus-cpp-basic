e3a33be9b18f57c3caf0f4f873e915178f92b4d8
vagnerserge <sergei.vagner@c3dlabs.com>
2024-09-26 22:46:12 +0300

    vector test

--- test/vectorTest.cpp
+++ test/vectorTest.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <vector>
 
-struct VectorFix : public testing::Test {
+struct VectorTest : public testing::Test {
 
   const size_t elementCount = 10;
   std::vector<size_t> vector; //the same vector instance for all test cases
@@ -16,27 +16,26 @@ struct VectorFix : public testing::Test {
 };
 
 
-
 TEST(vector, Create) {
   std::vector<int> vector;
   ASSERT_EQ(vector.size(), 0);
   ASSERT_TRUE(vector.empty());
 }
 
-TEST_F(VectorFix, PushBack) {
+TEST_F(VectorTest, PushBack) {
   vector.push_back(20);
   ASSERT_EQ(vector.size(), 11);
   ASSERT_EQ(vector.back(), 20);
 }
 
-TEST_F(VectorFix, PopBack) {
+TEST_F(VectorTest, PopBack) {
   vector.pop_back();
   ASSERT_EQ(vector.size(), 9);
   ASSERT_EQ(vector.back(), 8);
 
 }
 
-TEST_F(VectorFix, Insert) {
+TEST_F(VectorTest, Insert) {
   vector.insert(vector.begin() + 5, 60);
   ASSERT_EQ(vector.size(), 11); //вставка произошла
   // проверяем, что элемент 60 вставился между 4 и 5.
@@ -46,20 +45,20 @@ TEST_F(VectorFix, Insert) {
 
 }
 
-TEST_F(VectorFix, PushFront) {
+TEST_F(VectorTest, PushFront) {
   vector.insert(vector.begin(),20);
   ASSERT_EQ(vector.size(), 11);
   ASSERT_EQ(vector.front(), 20);
 }
 
-TEST_F(VectorFix, PopFront) {
+TEST_F(VectorTest, PopFront) {
   vector.erase(vector.begin());
   ASSERT_EQ(vector.size(), 9);
   ASSERT_EQ(vector.front(), 1);
 
 }
 
-TEST_F(VectorFix, Erase) {
+TEST_F(VectorTest, Erase) {
   vector.erase(vector.begin()+5);
   ASSERT_EQ(vector.size(), 9); //удаление произошло
   // проверяем, что удалился элемент между 4 и 6.
@@ -68,11 +67,11 @@ TEST_F(VectorFix, Erase) {
 
 }
 
-TEST_F(VectorFix, Size) {
+TEST_F(VectorTest, Size) {
   ASSERT_EQ(vector.size(), 10);
 }
 
-TEST_F(VectorFix, Getelement) {
+TEST_F(VectorTest, Getelement) {
   size_t arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
   size_t i = 0;
 
@@ -80,7 +79,7 @@ TEST_F(VectorFix, Getelement) {
     ASSERT_EQ(elem, arr[i++]); //can't understand where is failure
 }
 
-TEST_F(VectorFix, Clear) {
+TEST_F(VectorTest, Clear) {
   vector.clear();
   ASSERT_EQ(vector.size(), 0);
   ASSERT_TRUE(vector.empty());
